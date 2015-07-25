79badca35f16fc3f20a7c7e07088872e4913e88e
Patricia Garcia <pat@patriciagarcia.me>
2015-07-25 11:01:44 +0200

    test(config): allow specifying source columns
    For `Project`, `Client`, `Task`, `Tags`, allow to specify a column
    from the source CSV file to be directly used in the destination file.
    If a column is specified in this way it takes precedence over any set
    of rules (the set of rules will be ignored)

--- test/convert-test.js
+++ test/convert-test.js
@@ -26,4 +26,95 @@ describe('convert', function() {
       });
     });
   });
+  describe('Columns', function() {
+    beforeEach(function() {
+      config = {
+        email: 'test@example.com'
+      };
+    });
+    describe('Project', function() {
+      it('uses a column from the source CSV if specified in `config.projectColumn`', function() {
+        config.projectColumn = 'proj';
+        var sourceCsv = "proj,column2\ntest,test2";
+        var toggldCsv = "Email,Client,Project,Task,Description,Tags,Start date,Start time,Duration,Billable\r\ntest@example.com,,test,,,,,,00:00:00,Y";
+
+        var options = {
+          sourceCsv: sourceCsv,
+          config: config
+        };
+
+        convert(options, function(error, convertedCsv) {
+          if (error) {
+            throws(error);
+          }
+
+          assert.equal(error, null);
+          assert.equal(convertedCsv, toggldCsv);
+        });
+      });
+    });
+    describe('Client', function() {
+      it('uses a column from the source CSV if specified in `config.clientColumn`', function() {
+        config.clientColumn = 'client';
+        var sourceCsv = "client,column2\ntest,test2";
+        var toggldCsv = "Email,Client,Project,Task,Description,Tags,Start date,Start time,Duration,Billable\r\ntest@example.com,test,,,,,,,00:00:00,Y";
+
+        var options = {
+          sourceCsv: sourceCsv,
+          config: config
+        };
+
+        convert(options, function(error, convertedCsv) {
+          if (error) {
+            throws(error);
+          }
+
+          assert.equal(error, null);
+          assert.equal(convertedCsv, toggldCsv);
+        });
+      });
+    });
+    describe('Task', function() {
+      it('uses a column from the source CSV if specified in `config.taskColumn`', function() {
+        config.taskColumn = 'task';
+        var sourceCsv = "task,column2\ntest,test2";
+        var toggldCsv = "Email,Client,Project,Task,Description,Tags,Start date,Start time,Duration,Billable\r\ntest@example.com,,,test,,,,,00:00:00,Y";
+
+        var options = {
+          sourceCsv: sourceCsv,
+          config: config
+        };
+
+        convert(options, function(error, convertedCsv) {
+          if (error) {
+            throws(error);
+          }
+
+          assert.equal(error, null);
+          assert.equal(convertedCsv, toggldCsv);
+        });
+      });
+    });
+    describe('Tags', function() {
+      it('uses a column from the source CSV if specified in `config.tagsColumn`', function() {
+        config.tagsColumn = 'tags';
+        var sourceCsv = "tags,column2\ntest,test2";
+        var toggldCsv = "Email,Client,Project,Task,Description,Tags,Start date,Start time,Duration,Billable\r\ntest@example.com,,,,,test,,,00:00:00,Y";
+
+        var options = {
+          sourceCsv: sourceCsv,
+          config: config
+        };
+
+        convert(options, function(error, convertedCsv) {
+          if (error) {
+            throws(error);
+          }
+
+          assert.equal(error, null);
+          assert.equal(convertedCsv, toggldCsv);
+        });
+      });
+    });
+  });
 });
